0df02db4b9f435e7e88dc87de6759155e74e2a9b
nickAnhel <anhimovn1@gmail.com>
2024-03-21 03:03:36 +0300

    L4 Updted classes User and Queue

--- Lab4 Dunder Methods/task_2/my_queue.py
+++ Lab4 Dunder Methods/task_2/my_queue.py
@@ -1,55 +1,66 @@
-from typing import Sequence
+from typing import Sequence, Generator, Any
+
+
+# fmt: off
+class EmptyQueueError(Exception): ...
+# fmt: on
 
 
 class Queue:
     def __init__(self, items: Sequence | None = None) -> None:
         if items is None:
-            self.__items: list = []
+            self.__items: list[Any] = []
         else:
             self.__items = list(items)
 
     @property
-    def items(self):
+    def items(self) -> list[Any]:
         return self.__items
 
     @property
-    def size(self):
+    def size(self) -> int:
         return len(self.__items)
 
     @property
-    def empty(self):
-        return bool(self.__items)
+    def empty(self) -> bool:
+        return not bool(self.__items)
 
-    def push(self, item):
+    def push(self, item) -> None:
         self.__items.append(item)
 
-    def pop(self):
+    def pop(self) -> Any:
+        if not self.__items:
+            raise EmptyQueueError("Queue is empty")
+
         item = self.__items[0]
         self.__items.remove(item)
         return item
 
-    def __bool__(self):
+    def __bool__(self) -> bool:
         return bool(self.__items)
 
     def __len__(self):
         return len(self.__items)
 
-    def __iter__(self):
+    def __iter__(self) -> Generator[Any, Any, None]:
         for item in self.__items:
             yield item
 
-    def __contains__(self, item):
+    def __contains__(self, item) -> bool:
         return item in self.__items
 
-    def __add__(self, other):
+    def __add__(self, other) -> "Queue":
         if not isinstance(other, self.__class__):
             raise TypeError
         return Queue(self.items + other.items)
 
-    def __iadd__(self, other):
+    def __iadd__(self, other) -> "Queue":
         if not isinstance(other, self.__class__):
             raise TypeError
         return Queue(self.items + other.items)
 
     def __repr__(self) -> str:
+        return "Queue: " + " -> ".join(str(item) for item in self.__items[::-1])
+
+    def __str__(self) -> str:
         return "Queue: " + " -> ".join(str(item) for item in self.__items[::-1])
--- Lab4 Dunder Methods/task_9/user.py
+++ Lab4 Dunder Methods/task_9/user.py
@@ -1,16 +1,54 @@
+import re
 from typing import Any
+from accessify import private
+
+
+# fmt: off
+class NegativeOrZeroAgeError(Exception): ...
+class EmailPatternError(Exception): ...
+# fmt: on
 
 
 class User:
-    private_attrs = ("age", "gender")
+    __private_attrs: tuple[str] = ()
 
-    def __init__(self, name: str, age: int, gender: str, address: str) -> None:
-        self.name = name
-        self.age = age
-        self.gender = gender
-        self.address = address
+    @private
+    @classmethod
+    def fill_private_attrs_set(cls) -> None:
+        cls.__private_attrs = ("name", "age", "gender", "address")
+
+    def __init__(self, name: str, age: int, gender: str, address: str, email: str) -> None:
+        self.name: str = name
+        self.age: int = self.__validate_age(age)
+        self.gender: str = gender
+        self.address: str = address
+        self.email: str = self.__validate_email(email)
+        self.fill_private_attrs_set()
 
     def __setattr__(self, name: str, value: Any) -> None:
-        if name in self.private_attrs:
-            raise AttributeError
-        self.name = value
+        if name in self.__private_attrs:
+            raise AttributeError("You can't change the value of this attribute after initialization")
+        object.__setattr__(self, name, value)
+
+    @private
+    @classmethod
+    def __validate_age(cls, age) -> int:
+        if not isinstance(age, int):
+            raise TypeError("Age must be an 'int'")
+
+        if age <= 0:
+            raise NegativeOrZeroAgeError("Age must be greater then zero")
+
+        return age
+
+    @private
+    @classmethod
+    def __validate_email(cls, email) -> str:
+        if not isinstance(email, str):
+            raise TypeError("Email must be 'str'")
+
+        pattern = re.compile(r"[a-zA-Z0-9._%+-]+@[a-zA-z0-9-]+\.[a-z]{2, 4}")
+        if not re.match(pattern, email):
+            raise EmailPatternError("Email should match the pattern 'youremail@site.com' ")
+
+        return email
