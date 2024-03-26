91f64c6d3b37a7a4ab582afff833274a1896606b
nickAnhel <anhimovn1@gmail.com>
2024-03-26 14:00:56 +0300

    L4 Added typing to the MyQueue and tests

--- Lab4 Dunder Methods/task_2/my_queue.py
+++ Lab4 Dunder Methods/task_2/my_queue.py
@@ -1,21 +1,24 @@
 import copy
-from dataclasses import dataclass
-from typing import Mapping, Sequence, Generator, Any
+from typing import Generic, Sequence, Generator, TypeVar
+import timeit
+
+
+T = TypeVar("T")
 
 # fmt: off
 class EmptyQueueError(Exception): ...
 # fmt: on
 
 
-class MyQueue:
-    def __init__(self, items: Sequence[Any] | Mapping[Any, Any] | None = None) -> None:
+class MyQueue(Generic[T]):
+    def __init__(self, items: Sequence[T] | None = None) -> None:
         if items is None:
-            self.__items: list[Any] = []
+            self.__items: list[T] = []
         else:
-            self.__items = list(items)
+            self.__items: list[T] = list(items)
 
     @property
-    def items(self):
+    def items(self) -> list[T]:
         return copy.deepcopy(self.__items)
 
     @property
@@ -26,10 +29,10 @@ class MyQueue:
     def empty(self) -> bool:
         return len(self.__items) == 0
 
-    def push(self, item) -> None:
+    def push(self, item: T) -> None:
         self.__items.append(item)
 
-    def get(self) -> Any:
+    def get(self) -> T:
         if not self.__items:
             raise EmptyQueueError("Queue is empty.")
 
@@ -38,10 +41,10 @@ class MyQueue:
     def __bool__(self) -> bool:
         return bool(self.__items)
 
-    def __len__(self):
+    def __len__(self) -> int:
         return len(self.__items)
 
-    def __iter__(self) -> Generator[Any, Any, None]:
+    def __iter__(self) -> Generator[T, None, None]:
         for item in self.__items:
             yield item
 
@@ -68,3 +71,20 @@ class MyQueue:
 
     def __str__(self) -> str:
         return "Queue: " + " -> ".join(str(item) for item in self.__items[::-1])
+
+
+if __name__ == "__main__":
+    print(
+        timeit.timeit("[l.append(1) for _ in range(100)]", "l = []")
+    )
+    print(
+        timeit.timeit("[q.push(1) for _ in range(100)]", "from my_queue import MyQueue; q = MyQueue()")
+    )
+
+    queue = MyQueue()
+    for i in range(1, 10):
+        queue.push(i)
+
+    while not queue.empty:
+        task_index = queue.get()
+        print(f"Working on task #{task_index}")
--- Lab4 Dunder Methods/task_2/test_my_queue.py
+++ Lab4 Dunder Methods/task_2/test_my_queue.py
@@ -14,44 +14,44 @@ def fixture_empty_queue() -> MyQueue:
 
 
 # MyQueue tests
-def test_queue_init_and_props(queue):
+def test_queue_init_and_props(queue: MyQueue) -> None:
     assert queue.items == [1, 2, 3, 4]
     assert queue.empty is False
     assert queue.size == 4
 
 
-def test_queue_push(queue):
+def test_queue_push(queue: MyQueue) -> None:
     queue.push(5)
     assert queue.items == [1, 2, 3, 4, 5]
     assert queue.size == 5
 
 
-def test_queue_get(queue, empty_queue):
-    item = queue.get()
+def test_queue_get(queue: MyQueue, empty_queue: MyQueue) -> None:
+    item: int = queue.get()
     assert queue.items == [2, 3, 4]
     assert queue.size == 3
     assert item == 1
 
     with pytest.raises(EmptyQueueError):
-        empty_queue.pop()
+        empty_queue.get()
 
 
-def test_queue_to_bool(queue, empty_queue):
+def test_queue_to_bool(queue: MyQueue, empty_queue: MyQueue) -> None:
     assert bool(queue) is True
     assert bool(empty_queue) is False
 
 
-def test_queue_len(queue, empty_queue):
+def test_queue_len(queue: MyQueue, empty_queue: MyQueue) -> None:
     assert len(queue) == 4
     assert len(empty_queue) == 0
 
 
-def test_queue_contains(queue):
+def test_queue_contains(queue: MyQueue) -> None:
     assert (1 in queue) is True
     assert (5 in queue) is False
 
 
-def test_queue_add_iadd(queue):
+def test_queue_add_iadd(queue: MyQueue):
     assert queue + MyQueue([5, 6]) == MyQueue([1, 2, 3, 4, 5, 6])
 
     queue += MyQueue([5, 6])
