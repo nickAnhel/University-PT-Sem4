0425777be1eed38b79da59afefc577b5af237a0a
nickAnhel <anhimovn1@gmail.com>
2024-03-27 21:44:00 +0300

    L4 Fixed type anotations in tests

--- Lab4 Dunder Methods/task_2/test_my_queue.py
+++ Lab4 Dunder Methods/task_2/test_my_queue.py
@@ -5,29 +5,29 @@ from my_queue import EmptyQueueError, MyQueue
 
 # Fixtures
 @pytest.fixture(name="queue")
-def fixture_queue() -> MyQueue:
-    return MyQueue([1, 2, 3, 4])
+def fixture_queue() -> MyQueue[int]:
+    return MyQueue[int]([1, 2, 3, 4])
 
 
 @pytest.fixture(name="empty_queue")
-def fixture_empty_queue() -> MyQueue:
-    return MyQueue()
+def fixture_empty_queue() -> MyQueue[int]:
+    return MyQueue[int]()
 
 
 # MyQueue tests
-def test_queue_init_and_props(queue: MyQueue) -> None:
+def test_queue_init_and_props(queue: MyQueue[int]) -> None:
     assert queue.items == [1, 2, 3, 4]
     assert queue.empty is False
     assert queue.size == 4
 
 
-def test_queue_push(queue: MyQueue) -> None:
+def test_queue_push(queue: MyQueue[int]) -> None:
     queue.push(5)
     assert queue.items == [1, 2, 3, 4, 5]
     assert queue.size == 5
 
 
-def test_queue_get(queue: MyQueue, empty_queue: MyQueue) -> None:
+def test_queue_get(queue: MyQueue[int], empty_queue: MyQueue[int]) -> None:
     item: int = queue.get()
     assert queue.items == [2, 3, 4]
     assert queue.size == 3
@@ -37,23 +37,23 @@ def test_queue_get(queue: MyQueue, empty_queue: MyQueue) -> None:
         empty_queue.get()
 
 
-def test_queue_to_bool(queue: MyQueue, empty_queue: MyQueue) -> None:
+def test_queue_to_bool(queue: MyQueue[int], empty_queue: MyQueue[int]) -> None:
     assert bool(queue) is True
     assert bool(empty_queue) is False
 
 
-def test_queue_len(queue: MyQueue, empty_queue: MyQueue) -> None:
+def test_queue_len(queue: MyQueue[int], empty_queue: MyQueue[int]) -> None:
     assert len(queue) == 4
     assert len(empty_queue) == 0
 
 
-def test_queue_contains(queue: MyQueue) -> None:
+def test_queue_contains(queue: MyQueue[int]) -> None:
     assert (1 in queue) is True
     assert (5 in queue) is False
 
 
-def test_queue_add_iadd(queue: MyQueue) -> None:
-    assert queue + MyQueue([5, 6]) == MyQueue([1, 2, 3, 4, 5, 6])
+def test_queue_add_iadd(queue: MyQueue[int]) -> None:
+    assert queue + MyQueue[int]([5, 6]) == MyQueue[int]([1, 2, 3, 4, 5, 6])
 
-    queue += MyQueue([5, 6])
-    assert queue == MyQueue([1, 2, 3, 4, 5, 6])
+    queue += MyQueue[int]([5, 6])
+    assert queue == MyQueue[int]([1, 2, 3, 4, 5, 6])
